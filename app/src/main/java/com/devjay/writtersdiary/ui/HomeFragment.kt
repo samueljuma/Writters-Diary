b7709637815e4c515ac2aa52495d19255f0ab2c2
samueljuma <samueljuma1024@gmail.com>
2021-07-07 20:26:18 +0300

    HomeFragment binding

---
+++ app/src/main/java/com/devjay/writtersdiary/ui/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.devjay.writtersdiary.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.devjay.writtersdiary.R
+import com.devjay.writtersdiary.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_home,container, false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
+++ app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.devjay.writtersdiary
+package com.devjay.writtersdiary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devjay.writtersdiary.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
