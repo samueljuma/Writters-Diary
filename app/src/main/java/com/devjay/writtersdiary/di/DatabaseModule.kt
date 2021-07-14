1d7fdcb6166fbdbf23881719d70a0707194790d7
samueljuma <samueljuma1024@gmail.com>
2021-07-14 13:17:25 +0300

    di: modify database module

--- app/src/main/java/com/devjay/writtersdiary/di/DatabaseModule.kt
+++ app/src/main/java/com/devjay/writtersdiary/di/DatabaseModule.kt
@@ -1,2 +1,42 @@
 package com.devjay.writtersdiary.di
 
+import android.content.Context
+import com.devjay.writtersdiary.data.database.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase (@ApplicationContext context: Context): AppDatabase{
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    fun provideWriterDao(appDatabase: AppDatabase): WriterDao{
+        return appDatabase.writerDao
+    }
+
+    @Provides
+    fun provideWriterTaskDao(appDatabase: AppDatabase): WriterTaskDao{
+        return appDatabase.writerTaskDao
+    }
+
+    @Provides
+    fun provideClientDao(appDatabase: AppDatabase): ClientDao{
+        return appDatabase.clientDao
+    }
+
+    @Provides
+    fun provideClientTaskDao(appDatabase: AppDatabase): ClientTaskDao{
+        return appDatabase.clientTaskDao
+    }
+
+}
