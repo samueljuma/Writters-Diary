770521f378c186062fb66c82edd788b2b072b2df
samueljuma <samueljuma1024@gmail.com>
2021-08-13 19:13:30 +0300

    ClientTaskList Fragment and ViewModel initial setup

--- app/src/main/java/com/devjay/writtersdiary/viewmodels/ClientTaskListViewModel.kt
+++ app/src/main/java/com/devjay/writtersdiary/viewmodels/ClientTaskListViewModel.kt
@@ -1,4 +1,19 @@
 package com.devjay.writtersdiary.viewmodels
 
-class ClientTaskListViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devjay.writtersdiary.data.entities.ClientTask
+import com.devjay.writtersdiary.data.repository.ClientTaskRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ClientTaskListViewModel @Inject constructor(
+    private val clientTaskRepository: ClientTaskRepository
+): ViewModel(){
+
+    fun getAllClientsTasks(clientId: Long): LiveData<List<ClientTask>>{
+        return clientTaskRepository.getAllClientsTasks(clientId).asLiveData()
+    }
 }
