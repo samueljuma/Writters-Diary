959e8208f3bbe504b668c4c456b862fca9698c19
samueljuma <samueljuma1024@gmail.com>
2021-08-13 21:49:31 +0300

    display ClientTask

--- app/src/main/java/com/devjay/writtersdiary/ui/ClientTaskListFragment.kt
+++ app/src/main/java/com/devjay/writtersdiary/ui/ClientTaskListFragment.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.devjay.writtersdiary.R
+import com.devjay.writtersdiary.adpters.ClientTaskListAdapter
+import com.devjay.writtersdiary.adpters.WriterTaskListAdapter
 import com.devjay.writtersdiary.databinding.FragmentClientTaskListBinding
+import com.devjay.writtersdiary.databinding.FragmentWriterTaskListBinding
 import com.devjay.writtersdiary.viewmodels.ClientTaskListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +26,18 @@ class ClientTaskListFragment : Fragment() {
     ): View? {
         binding = FragmentClientTaskListBinding.inflate(inflater,container,false)
 
+        val adapter = ClientTaskListAdapter()
+        binding.viewModel = viewModel
+
+        subscribeUI(adapter,binding,1)
         return binding.root
+    }
+
+    private fun subscribeUI(adapter: ClientTaskListAdapter, binding: FragmentClientTaskListBinding, clientId: Long){
+        viewModel.getAllClientsTasks(clientId).observe(viewLifecycleOwner){ result ->
+            binding.hasTasks = !result.isNullOrEmpty()
+            adapter.submitList(result)
+        }
     }
 
 }
