206c839d55b839206b7ffc981ce70cd18fc6cb66
samueljuma <samueljuma1024@gmail.com>
2021-08-13 19:03:01 +0300

    modify WriterTaskFragment and ViewModel

--- app/src/main/java/com/devjay/writtersdiary/viewmodels/WriterTaskListViewModel.kt
+++ app/src/main/java/com/devjay/writtersdiary/viewmodels/WriterTaskListViewModel.kt
@@ -1,4 +1,19 @@
 package com.devjay.writtersdiary.viewmodels
 
-class WriterTaskListViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devjay.writtersdiary.data.entities.WriterTask
+import com.devjay.writtersdiary.data.repository.WriterTaskRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WriterTaskListViewModel @Inject constructor(
+    private val writerTaskRepository: WriterTaskRepository
+): ViewModel(){
+
+    fun getAllWriterTasks(writerId: Long): LiveData<List<WriterTask>>{
+        return writerTaskRepository.getAllWritersTasks(writerId).asLiveData()
+    }
 }
