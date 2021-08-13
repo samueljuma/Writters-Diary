87b37b141624ae844d376ffa43996e28ff41b07f
samueljuma <samueljuma1024@gmail.com>
2021-08-13 19:49:20 +0300

    set up TaskListAdapters

--- app/src/main/java/com/devjay/writtersdiary/adpters/ClientTaskListAdapter.kt
+++ app/src/main/java/com/devjay/writtersdiary/adpters/ClientTaskListAdapter.kt
@@ -1,4 +1,49 @@
 package com.devjay.writtersdiary.adpters
 
-class ClientTaskListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.devjay.writtersdiary.data.entities.ClientTask
+import com.devjay.writtersdiary.databinding.ClientTaskCardBinding
+
+class ClientTaskListAdapter: ListAdapter<ClientTask, ClientTaskListAdapter.ViewHolder>(ClientTaskDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position)!!)
+    }
+
+    // viewHolder
+    class ViewHolder (val binding: ClientTaskCardBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind (item: ClientTask){
+            binding.clientTask = item
+            binding.executePendingBindings()
+        }
+        companion object{
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ClientTaskCardBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    class ClientTaskDiffCallback: DiffUtil.ItemCallback<ClientTask>(){
+        override fun areItemsTheSame(oldItem: ClientTask, newItem: ClientTask): Boolean {
+            return oldItem.taskID == newItem.taskID
+        }
+
+        override fun areContentsTheSame(oldItem: ClientTask, newItem: ClientTask): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
 }
+
+
--- app/src/main/java/com/devjay/writtersdiary/adpters/WriterTaskListAdapter.kt
+++ app/src/main/java/com/devjay/writtersdiary/adpters/WriterTaskListAdapter.kt
@@ -1,4 +1,49 @@
 package com.devjay.writtersdiary.adpters
 
-class WriterTaskListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.devjay.writtersdiary.data.entities.WriterTask
+import com.devjay.writtersdiary.databinding.WriterTaskCardBinding
+
+class WriterTaskListAdapter: ListAdapter<WriterTask, WriterTaskListAdapter.ViewHolder>(WriterTaskDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position)!!)
+    }
+
+    // viewHolder
+    class ViewHolder (val binding: WriterTaskCardBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind (item: WriterTask){
+            binding.writerTask = item
+            binding.executePendingBindings()
+        }
+        companion object{
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = WriterTaskCardBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    class WriterTaskDiffCallback: DiffUtil.ItemCallback<WriterTask>(){
+        override fun areItemsTheSame(oldItem: WriterTask, newItem: WriterTask): Boolean {
+            return oldItem.taskID == newItem.taskID
+        }
+
+        override fun areContentsTheSame(oldItem: WriterTask, newItem: WriterTask): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
 }
+
+
