a3ce322e9145401512ef13da10d4c062566b7032
samueljuma <samueljuma1024@gmail.com>
2021-08-18 12:08:25 +0300

    add overflow menu

--- app/src/main/java/com/devjay/writtersdiary/ui/HomeFragment.kt
+++ app/src/main/java/com/devjay/writtersdiary/ui/HomeFragment.kt
@@ -1,15 +1,15 @@
 package com.devjay.writtersdiary.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.devjay.writtersdiary.R
 import com.devjay.writtersdiary.databinding.FragmentHomeBinding
 import com.devjay.writtersdiary.viewmodels.HomeViewModel
@@ -34,6 +34,8 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        setHasOptionsMenu(true)
 
         binding.lifecycleOwner =this
 
@@ -61,5 +63,14 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
+    }
 }
