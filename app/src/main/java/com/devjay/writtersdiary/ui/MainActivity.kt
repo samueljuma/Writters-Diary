95abd470a5f0c2801905d27f596151e2aa219cfb
samueljuma <samueljuma1024@gmail.com>
2021-08-18 10:44:47 +0300

    added navigateUp

--- app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
+++ app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.devjay.writtersdiary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.devjay.writtersdiary.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
