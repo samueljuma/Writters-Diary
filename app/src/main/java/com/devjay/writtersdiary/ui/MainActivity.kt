886e73a463a4aaa3028e2ae03371cb9cd5246ca3
samueljuma <samueljuma1024@gmail.com>
2021-08-19 15:45:20 +0300

    add navigation drawer

--- app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
+++ app/src/main/java/com/devjay/writtersdiary/ui/MainActivity.kt
@@ -2,23 +2,37 @@ package com.devjay.writtersdiary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.devjay.writtersdiary.R
+import com.devjay.writtersdiary.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController )
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
     }
 }
